17c7a87a36ff7dac2a8c4bfe3ccd1dccb0666c95
KIMJAEMIN <l3lue0515@naver.com>
2024-10-27 15:25:02 +0900

    git_Test_1_241027

---
+++ src/main/java/com/startwith906/post/PostApplication.java
@@ -0,0 +1,16 @@
+package com.startwith906.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostApplication.class, args);
+
+		System.out.println("### TEST ###");
+
+
+	}
+}
